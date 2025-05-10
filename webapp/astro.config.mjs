c6115dbb47a3fb4634fd5cef6e72d4812d80faf0
Hanif-adedotun <hanif.adedotun@gmail.com>
2025-05-10 04:02:56 +0100

    Init astro

---
+++ webapp/astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
+import react from '@astrojs/react';
+
+export default defineConfig({
+     integrations: [tailwind(), react()],
+     output: 'server',
+     adapter: vercel()
+}); 

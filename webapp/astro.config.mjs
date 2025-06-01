4e7401789e65f6d7add4d420fae91fdd6d3189c8
Hanif-adedotun <hanif.adedotun@gmail.com>
2025-06-01 01:20:45 +0100

    new bg color and font

--- webapp/astro.config.mjs
+++ webapp/astro.config.mjs
@@ -1,17 +1,25 @@
 import { defineConfig } from 'astro/config';
 
 import cloudflare from "@astrojs/cloudflare";
+import tailwindcss from "@tailwindcss/vite";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+
   adapter: cloudflare({
     mode: "directory"
   }),
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   experimental: {
     session: true
-  }
+  },
+
+  integrations: [react()]
 });

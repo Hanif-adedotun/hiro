fb91947dab4fc0d1c3953bfba6203b43f83d0c79
Hanif-adedotun <hanif.adedotun@gmail.com>
2025-06-06 23:45:02 +0100

    created single file for test

---
+++ server/github-single.py
@@ -0,0 +1,7 @@
+# {'owner': 'Hanif-adedotun', 'repo': 'semra-website'}\
+     
+# file path
+# src/app/_components/(landing)/(prayers)/prayers.tsx
+# src/app/contact/page.tsx
+# src/app/articles/page.tsx
+# src/app/articles/[id]/page.tsx

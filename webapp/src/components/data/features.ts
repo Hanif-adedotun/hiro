7dbfacd69b492ed6e522ab012d6f4cbac25d1e68
Hanif-adedotun <hanif.adedotun@gmail.com>
2025-06-30 13:12:09 +0100

    revising features section

---
+++ webapp/src/components/data/features.ts
@@ -0,0 +1,17 @@
+export const featureList = [
+  {
+    title: "Better, together.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.",
+  },
+  {
+    title: "Better, together.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.",
+  },
+  {
+    title: "Better, together.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.",
+  },
+];
